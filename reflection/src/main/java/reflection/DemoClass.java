0c4aed5c928920d13579c5752510e2e1ce6a7dfb
hexhoc <hexhoc@gmail.com>
2021-11-17 18:55:32 +0300

    Refactoring

---
+++ reflection/src/main/java/reflection/DemoClass.java
@@ -0,0 +1,29 @@
+package reflection;
+
+import lombok.Data;
+import reflection.annotations.SimpleAnnotation;
+
+
+@Data
+public class DemoClass {
+    public int publicField;
+    private String value = "initValue";
+
+    public DemoClass(String value) {
+        this.value = value;
+    }
+
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    @SimpleAnnotation(Name = "Test")
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
+
+    private void privateMethod() {
+        System.out.println(value);
+        System.out.println("privateMethod executed");
+    }
+}
