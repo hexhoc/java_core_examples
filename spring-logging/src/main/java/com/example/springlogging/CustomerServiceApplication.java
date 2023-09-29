37e6f3261925cd9227714d86bf74e3b378b3cbb7
hexhoc <zhuravskiy_vv@nlmk.com>
2023-09-29 23:35:43 +0300

    Add spring logging

---
+++ spring-logging/src/main/java/com/example/springlogging/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
